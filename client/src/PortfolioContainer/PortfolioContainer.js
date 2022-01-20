7585b65defce6eb4bf5e7301ad31375df8c0b1e9
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-19 22:57:31 -0300

    Correção no scroll do contato

--- client/src/PortfolioContainer/PortfolioContainer.js
+++ client/src/PortfolioContainer/PortfolioContainer.js
@@ -1,23 +1,11 @@
-import React from "react";
-import { TOTAL_SCREENS } from "../utilities/commonUtils";
-import { FadeInSection } from "./FadeInSection/FadeInSection";
+import React from 'react'
+import { TOTAL_SCREENS } from '../utilities/commonUtils'
+import { FadeInSection } from './FadeInSection/FadeInSection'
 
 export default function PortfolioContainer() {
   const mapAllScreens = () => {
     return TOTAL_SCREENS.map((screen) => {
-      return screen.shouldFade ? (
-        <FadeInSection>
-          {screen.component ? (
-            <screen.component
-              screenName={screen.screen_name}
-              key={screen.screen_name}
-              id={screen.screen_name}
-            />
-          ) : (
-            <div id={screen.screen_name} key={screen.screen_name}></div>
-          )}
-        </FadeInSection>
-      ) : screen.component ? (
+      const Component = screen.component ? (
         <screen.component
           screenName={screen.screen_name}
           key={screen.screen_name}
@@ -25,9 +13,15 @@ export default function PortfolioContainer() {
         />
       ) : (
         <div id={screen.screen_name} key={screen.screen_name}></div>
-      );
-    });
-  };
+      )
 
-  return <div>{mapAllScreens()}</div>;
+      return screen.shouldFade ? (
+        <FadeInSection>{Component}</FadeInSection>
+      ) : (
+        Component
+      )
+    })
+  }
+
+  return <div>{mapAllScreens()}</div>
 }
