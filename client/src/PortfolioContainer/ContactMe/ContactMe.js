1a8bf371dc60cc59b5c7996e358cb258b85b9ae0
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2022-01-24 15:47:33 -0300

    mudando config do heroku

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -39,7 +39,7 @@ export default function ContactMe(props) {
         message,
       };
       setBool(true);
-      const res = await axios.post(`/contact`, data);
+      const res = await axios.post(`https://portfolio-herbert.herokuapp.com:5000/contact`, data);
       if (name.length === 0 || email.length === 0 || message.length === 0) {
         setBanner(res.data.msg);
         toast.error(res.data.msg);
